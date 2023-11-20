749bd25000ea88d5bebc29b91c9c0c92e0bd0c2e
Trotteriniku <stefanozidda3@gmail.com>
2023-11-20 13:28:59 +0100

    Milestone 4 header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <input type="text" v-model="data" @keyup.enter="getParams()">
-    </div>
+    <header class=" px-5 py-3 d-flex justify-content-between align-items-center position-sticky">
+        <img src="../assets/images/BOOLFIXIMG.png" alt="boolflix logo" style="width: 110px;">
+        <i class="fa-solid fa-search text-light fs-5 d-inline-block" v-if="!barExpand" @click="barExpand= true"></i>
+        <div class="border border-light p-1 " v-if="barExpand" style="max-width: 260px; min-width: 260px;" >
+        <i class="fa-solid fa-search text-light fs-5  d-inline-block " @click="barExpand= false"></i>
+
+        <input type="text" class="ps-3 align-baseline  border-0 text-secondary" id="searchinput" v-model="data" @keyup.enter="getParams()" placeholder="Titoli,persone,generi">
+        </div>
+    </header>
+    
 </template>
 
 <script>
@@ -10,6 +17,7 @@ import { store  } from '../data/store';
         name:'HeaderComponent',
         data(){
             return{
+                barExpand: false,
                 data:'',
                 store
             }
@@ -18,11 +26,30 @@ import { store  } from '../data/store';
             getParams(){
                 this.store.params.query= this.data
                 this.$emit('change-value')
+            },
+            barOpener(){
+                if (condition) {
+                    
+                }
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+    header{
+        // background-color: #222222;
+        background-color: transparent;
+        height: 68px;
+        background-image: linear-gradient(180deg,rgba(0,0,0,.7) 10%,transparent);
 
+        div{
+            background-color: #2222226a;
+        }
+
+        input{
+            background-color: #2222226a;
+            outline: none;
+        }
+    }
 </style>
